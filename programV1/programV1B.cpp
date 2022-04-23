534469c8aa12eec67e2b987be58abb1121778604
brianvansteen <bvansteen@gmail.com>
2022-04-23 11:07:46 -0400

    April23

--- programV1/programV1B.cpp
+++ programV1/programV1B.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
-using namespace std;
 
 void printHello()
 {
-	cout << "Hello!" << endl;
+	std::cout << "Hello from other file!" << std::endl <<std::endl;
+	char c = 0;
+	std::cout << "char: " << sizeof(char) << std::endl << std::endl;
+	signed int i = 0;
+	std::cout << "signed int: " << sizeof(int) << std::endl << std::endl;
+	float f = 0.0f;
+	std::cout << "float: " << sizeof(float) << std::endl << std::endl;
+	long double ld = 0.0;
+	std::cout << "long double: " << sizeof(ld) << std::endl << std::endl;
+
 }
